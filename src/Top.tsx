9d6789576032d9ea9f8c5eeb9d7b01620059270d
ryonasso <ryounaoyuki@gmail.com>
2021-05-05 16:31:49 +0900

    refactor : refactoring Top.tsx

--- src/Top.tsx
+++ src/Top.tsx
@@ -9,7 +9,6 @@ export const Top = () => {
         <div className="col-span-1 bg-blue-400 h-screen justify-center flex items-center">
           <h1 className="text-white block text-5xl">笑ってはいけない○○時</h1>
         </div>
-        {/* <div className="bg-white"></div> */}
         <div className="text-center flex items-center justify-center">
           <Link
             className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded block"
